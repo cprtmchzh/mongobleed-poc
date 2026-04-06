445152c36131754d5fc488330dc0d0d30ec53277
cprtmchzh <bin54511563@gmail.com>
2026-04-06 17:27:34 +0900

    fix: 도커 설정 일부 변경

--- init/init-mongo.py
+++ init/init-mongo.py
@@ -1,6 +1,8 @@
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://localhost:27017/')
+client = MongoClient(
+    "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000"
+)
 db = client['testdb']
 userInfo = db['userInfo']
 userToken = db['token']
